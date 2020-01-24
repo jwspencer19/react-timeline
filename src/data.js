f2bb8c25cd194e8ae631bc72747bd53bbe94db60
John Spencer <spencer19@yahoo.com>
2020-01-24 17:00:37 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+    text: "This is the first smart outcome",
+    date: "Jan 29, 2020",
+    category : {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url: '/details/01',
+      text: 'More'
+    }
+  },
+  {
+    text: "This is the second smart outcome",
+    date: "Jan 30, 2020",
+    category : {
+      tag: 'app-ideas',
+      color: '#FFDB14'
+    },
+    link: {
+      url: '/details/02',
+      text: 'More'
+    }
+  },
+  {
+    text: "This is the third smart outcome",
+    date: "Jan 30, 2020",
+    category : {
+      tag: 'blog',
+      color: '#e17b77'
+    },
+    link: {
+      url: '/details/03',
+      text: 'More'
+    }
+  },
+  {
+    text: "This is the another smart outcome",
+    date: "Jan 30, 2020",
+    category : {
+      tag: 'blog',
+      color: '#e17b77'
+    },
+    link: {
+      url: '/details/03',
+      text: 'More'
+    }
+  },
+  {
+    text: "This is the another smart outcome",
+    date: "Jan 30, 2020",
+    category : {
+      tag: 'blog',
+      color: '#e17b77'
+    },
+    link: {
+      url: '/details/03',
+      text: 'More'
+    }
+  }
+]
+
+export default data;
